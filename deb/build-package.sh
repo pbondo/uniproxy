f4861e3f38de4d8bdbcbd8f982dac8863d53dd5b
Poul Bondo <poul.bondo@gmail.com>
2013-09-09 12:13:42 +0200

    Build ubuntu package

--- deb/build-package.sh
+++ deb/build-package.sh
@@ -1,6 +1,10 @@
+#
+# Build debian/ubuntu package using checkinstall.
+#
 #!/bin/bash
 
-#run this with sudo. It will remove some of the annoying side effects afterwards.
+version=0.0.2
+release=1
 
 ln -s ../deb/postinstall-pak
 ln -s ../deb/preinstall-pak
@@ -9,8 +13,10 @@ ln -s ../deb/postremove-pak
 ln -s ../deb/description-pak
 touch install_manifest.txt
 
-sudo checkinstall --install=no --pkgversion=0.0.1 --pkglicense=GPL --pkgname=uniproxy --maintainer="Poul Bondo \<pba@gatehouse.dk\>" --provides=uniproxy --requires="openssl" --nodoc --exclude=/home  --backup=no
-sudo rm /etc/init/uniproxy.conf
+make
+
+#run this with sudo. It will remove some of the annoying side effects afterwards.
+sudo checkinstall --install=no --pkgversion=${version} --pkgrelease=${release} --pkglicense=GPL --pkgname=uniproxy --maintainer="Poul Bondo \<pba@gatehouse.dk\>" --provides=uniproxy --requires="openssl" --nodoc --exclude=/home  --backup=no
+#sudo rm /etc/init/uniproxy.conf
 
 rm postinstall-pak  postremove-pak  preinstall-pak  preremove-pak description-pak
-
